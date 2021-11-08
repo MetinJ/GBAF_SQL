ba2cb5834fde86aabff5ff05e5dcaf3f1858ab60
MetinJ <93438202+MetinJ@users.noreply.github.com>
2021-11-08 15:17:31 +0100

    Ajout du pied de page

---
+++ footer.php
@@ -0,0 +1,12 @@
+ </div>
+  </div>
+</div>
+<footer id="footer">
+  <ul>
+    <li>© 2021 |  &nbsp;</li>
+    <li>Mentions légales |  &nbsp;</li>
+    <li>Contact</li>
+  </ul>
+</footer>
+</body>
+</html>
